18579006cebf460debf66d6544f60bb598116780
idolago94 <idolago94@gmail.com>
2019-01-27 14:20:59 +0200

    fix mark pipe

--- shoppingWeb-front/src/app/pipes/milk.pipe.ts
+++ shoppingWeb-front/src/app/pipes/milk.pipe.ts
@@ -1,17 +1,20 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Pipe({
   name: 'milk'
 })
 export class MilkPipe implements PipeTransform {
 
+  constructor( private sanitizer:DomSanitizer ){}
+
   transform(value: string, args: string): any {
     if(args && value.indexOf(args)!=-1 ) {
       let index = value.indexOf(args);
-      let newValue  = value.slice(0,index) + `<mark>` + value.slice(index, index+args.length) + "</mark>" + value.slice(index+args.length, value.length);
-      return newValue;
+      let newValue  = value.slice(0,index) + `<mark style='background-color:yellow'>` + value.slice(index, index+args.length) + "</mark>" + value.slice(index+args.length, value.length);
+      return this.sanitizer.bypassSecurityTrustHtml(newValue);
     }
-    return value;
+    return this.sanitizer.bypassSecurityTrustHtml(value);
   }
 
 }
